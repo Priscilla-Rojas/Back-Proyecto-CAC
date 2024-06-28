39843acb3a6ecd65cb29a5b026ad6edc49ed53cb
Daniela Bastias <danybastias@outlook.com>
2024-06-27 23:32:10 -0300

    Conectadndo a la BBDD

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const app = require('./src/app');
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`**Servidor corriendo en el puerto ${PORT}**`);
 });
