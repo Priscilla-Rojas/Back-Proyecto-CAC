8e31b4689cb93f2035375b4247c57f575b4bb1ee
Priscilla Rojas <73943767+Priscilla-Rojas@users.noreply.github.com>
2024-07-10 18:15:29 -0300

    preparando todo para el despliegue en produccio

--- config/config.js
+++ config/config.js
@@ -1,4 +1,9 @@
 module.exports = {
   secretKey: process.env.SECRET_KEY,
-  tokenExpiresIn: '1h'
+  tokenExpiresIn: '1h',
+  PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
 }
